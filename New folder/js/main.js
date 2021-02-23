e1a0975ebfb0953ad93cc2e7d123d94452f687bd
codekiller-star <78665956+codekiller-star@users.noreply.github.com>
2021-02-24 03:16:59 +0600

    Add files via upload

---
+++ New folder/js/main.js
@@ -0,0 +1,9 @@
+// Initialize the slider
+
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+	$('#main_menu').meanmenu({
+		meanMenuContainer:'#mobile_menu',
+		meanScreenWidth: 991,
+	});
+  });
